e9506f017cd414307a1d49844e23a7b25acf3d44
Thomas Nicoue <tnicoue@c1r3p7.42nice.fr>
2022-01-31 14:53:48 +0100

    31 02 2022

---
+++ ft_lstadd_back.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnicoue <tnicoue@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 14:39:45 by tnicoue           #+#    #+#             */
+/*   Updated: 2022/01/31 13:36:15 by tnicoue          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_lstadd_back(t_stack **alst, t_stack *new)
+{
+	t_stack	*last;
+	t_stack *tmp;
+	
+	tmp = *alst;
+	if (alst)
+	{
+		if (*alst == NULL)
+			*alst = new;
+		else
+		{
+			while (tmp->next)
+				tmp = tmp->next;
+			last = tmp;
+			last->next = new;
+		}
+	}
+}
+
+void	ft_init_struct(t_stock *stack)
+{
+	stack->chunk_size = 0;
+	stack->bigger = INT_MIN;
+	stack->shorter = INT_MAX;
+	stack->shorter2 = INT_MAX;
+	stack->pos_shorter = 0;
+	stack->pos_shorter2 = 0;
+	stack->size_b = 0;
+	stack->size_a = 0;
+	stack->lastb = 0;
+	stack->size_all = 0;
+	stack->var_tabcheck = 0;
+	stack->pos_bigger = 0;
+	stack->done = 0;
+	stack->checkaccepta = 0;
+	stack->checklast = 0;
+}
---
+++ ft_sort.c
@@ -0,0 +1,686 @@
+
+
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevyn <kevyn@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 09:25:18 by tnicoue           #+#    #+#             */
+/*   Updated: 2022/01/25 18:26:20 by kevyn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+void get_bigger_b(t_stack *)
+{
+    int i;
+
+    i = 0
+    while (stack_b)
+    {
+        if(bigger < stack_b->content)
+            bigger = stack_b->content;
+        stack_b = stack_b->next;
+    }
+    while (t_stack * t_stack_b)
+    {
+        if(bigger == t_stack_b->content)
+            pos_bigger == i;
+        i++;
+        t_stack_b = t_stack_b->next;       
+    }
+    size_b = i;
+}
+
+void get_shorter_a(t_stack *)
+{
+    t_stack *tmp;
+    int i;
+
+    tmp = t_stack;
+    while (tmp)
+    {
+        if(shorter > tmp->content && (i >= 0 && i <= 10)
+            shorter = tmp->content;
+        if(shorter2 > tmp->content && (i >= (size_a - 10) && i <= size_a)
+            shorter2 = tmp->content;
+        tmp = tmp->next;
+    }
+    i = 1;
+    while (t_stack * t_stack_a)
+    {
+        if(shorter == t_stack_a->content)
+            pos_shorter == i;
+        if(shorter2 == t_stack_a->content)
+            pos_shorter2 == i;
+        i++;
+        t_stack_a = t_stack_a->next;       
+    }
+}
+
+
+int get_value(int i, t_stack *stack, int *value)
+{
+    int w;
+    
+    w = 0;
+    while(w < i)
+        t_stack = t_stack->next;
+    value[i] = t_stack->content;
+}
+*/
+void    direction(t_stack **stack_a, t_stack **stack_b, t_stock *stock)
+{
+    stock->size_before =get_size(stack_a, stock);
+    if (stock->size_all <= 3)
+        choose_sort_short(stack_a, stock);
+    else
+        choose_sort_big(stack_a, stack_b, stock);
+}
+    
+int get_size(t_stack **stack, t_stock *stock)
+{
+    t_stack *tmp;
+    int     i;
+    
+    tmp = (*stack);
+    i = 0;
+    while(tmp)
+    {
+        i++;
+        tmp = tmp->next;
+    }
+    stock->size_all = i;
+    return (i);
+}
+
+void choose_sort_short(t_stack **stack_a, t_stock *stack)
+{
+    if(stack->size_all == 2)
+        ft_tri2(stack_a);
+    else if(stack->size_all == 3)
+        ft_tri3(stack_a);
+}
+
+void ft_tri2(t_stack **stack_a)
+{
+    if((*stack_a)->content > (*stack_a)->next->content)
+        ft_sa(stack_a);
+}
+
+void ft_tri3(t_stack **stack_a)
+{
+    if((*stack_a)->content < (*stack_a)->next->content 
+        && (*stack_a)->content < (*stack_a)->next->next->content
+        && (*stack_a)->next->content > (*stack_a)->next->next->content) //132
+    {
+        ft_sa(stack_a);
+        ft_ra(stack_a);
+    }
+    else if((*stack_a)->content < (*stack_a)->next->content 
+        && (*stack_a)->content > (*stack_a)->next->next->content) //231
+        ft_rra(stack_a);
+    else if ((*stack_a)->content > (*stack_a)->next->content 
+        && (*stack_a)->content > (*stack_a)->next->next->content
+        && (*stack_a)->next->content < (*stack_a)->next->next->content) //312
+        ft_ra(stack_a);
+    else if ((*stack_a)->content > (*stack_a)->next->content 
+        && (*stack_a)->next->content < (*stack_a)->next->next->content) //213
+        ft_sa(stack_a);
+    else if((*stack_a)->content > (*stack_a)->next->content 
+        && (*stack_a)->content > (*stack_a)->next->next->content
+        && (*stack_a)->next->content > (*stack_a)->next->next->content) //321
+    {
+        ft_sa(stack_a);
+        ft_rra(stack_a);
+    }
+    else if ((*stack_a)->content < (*stack_a)->next->content
+        && (*stack_a)->content < (*stack_a)->next->next->content
+        && (*stack_a)->next->content < (*stack_a)->next->next->content)
+        return ;                                                        //123
+}
+
+void choose_sort_big(t_stack **stack_a, t_stack **stack_b, t_stock *stock)
+{
+    if(stock->size_all == 4)
+        ft_tri4(stack_a, stack_b, stock);
+    else if(stock->size_all == 5)
+        ft_tri5(stack_a, stack_b, stock);
+    else if(stock->size_all <= 100)
+        ft_tri100(stack_a, stack_b, stock);
+    /*else if (stock->size_all > 100 && stock->size_all <= 500)
+        ft_big_sort500(stack_a, stack_b, stock);*/
+}
+
+void getlower4_5(t_stack **stack_a, t_stock *stock)
+{
+    t_stack *tmp;
+    int i;
+
+    i = 0;
+    tmp = (*stack_a);
+    while (tmp)
+    {
+        if(stock->shorter > tmp->content)
+        {
+           stock->shorter = tmp->content;
+           stock->pos_shorter = i;
+        }
+        tmp = tmp->next;
+        i++;
+    }
+}
+
+void mvlower4(t_stack **stack_a, t_stack **stack_b, t_stock *stock)
+{
+    int i;
+
+    i = stock->pos_shorter;
+    if (i == 3)
+    {
+        ft_rra(stack_a);
+        ft_pb(stack_a, stack_b);
+    }
+    else
+    {
+        while (i != 0)
+        {
+            ft_ra(stack_a);
+            i--;
+        }
+        ft_pb(stack_a, stack_b);
+    }
+}
+ 
+void    ft_tri4(t_stack **stack_a, t_stack **stack_b, t_stock *stock)
+{
+    getlower4_5(stack_a, stock);
+    mvlower4(stack_a, stack_b, stock);
+    ft_tri3(stack_a);
+    ft_pa(stack_a, stack_b);
+}
+
+void    ft_tri5(t_stack **stack_a, t_stack **stack_b, t_stock *stock)
+{
+    getbigger5(stack_a, stock);
+    mvbigger5(stack_a, stack_b, stock);
+    getlower4_5(stack_a, stock);
+    mvlower4(stack_a, stack_b, stock);
+    ft_tri3(stack_a);
+    ft_pa(stack_a, stack_b);
+    ft_pa(stack_a, stack_b);
+    ft_ra(stack_a);
+}
+
+void    mvbigger5(t_stack **stack_a, t_stack **stack_b, t_stock *stock)
+{
+    int count;
+
+    count = 5 - stock->pos_bigger;
+    if (stock->pos_bigger > 2)
+    {
+        while(count > 0)
+        {
+            ft_rra(stack_a);
+            count--;
+        }
+        ft_pb(stack_a, stack_b);
+    }
+    else
+    {
+        while (stock->pos_bigger)
+        {
+            ft_ra(stack_a);
+            stock->pos_bigger--;
+        }
+        ft_pb(stack_a, stack_b);
+    }
+}
+
+void    getbigger5(t_stack **stack_a, t_stock *stock)
+{
+    int i;
+
+    i = 0;
+    t_stack *tmp;
+    tmp = (*stack_a);
+    while (tmp)
+    {
+        if(stock->bigger < tmp->content)
+        {
+            stock->bigger = tmp->content;
+            stock->pos_bigger = i;
+        }
+        tmp = tmp->next;
+        i++;
+    }
+}
+
+void get_size_a(t_stack **stack_a, t_stock *stock)
+{
+    int i;
+    t_stack *tmp;
+
+    i = 1;
+    tmp = (*stack_a);
+    while (tmp)
+    {
+        tmp = tmp->next;
+        i++;
+    }
+    stock->size_a = i;
+}
+
+void    ft_tri100(t_stack **stack_a, t_stack **stack_b, t_stock *stock)
+{
+    int indic;
+    int count;
+    int i;
+	int var;
+
+    i = 1;
+	var = 0;
+    
+	while(stock->done != stock->size_all)
+    {
+        if (stock->done + 20 > stock->size_all)
+            count = 20 - (stock->done - stock->size_all);
+        else
+            count = 20;
+        while(count != 0)
+        {
+            stock->size_b = get_size(stack_b, stock);
+            stock->size_a = get_size(stack_a, stock);
+            printf("boucle :%d\n", i++);
+            ft_crea_tab_pos(stack_a, stock);
+            printf("m\n");
+            nmkcoup(stock);
+            printf("n\n");
+            indic = checklowercoup(stock);
+            printf("o\n");
+            mvlowercoup(indic, stock, stack_a);
+            printf("p\n");
+            ft_last(stack_b, stock);
+            printf("q\n");
+            ft_pos_accept(stack_a, stack_b, stock);
+            count--;
+			ft_print_lst(*stack_b);
+        }
+        stock->size_a = get_size(stack_a, stock);
+        ft_accept_a(stack_a, stock);
+        while(stack_b)
+        {
+            ft_pb(stack_a, stack_b);
+        }
+    }
+}
+
+void ft_accept_a(t_stack **stack_a, t_stock *stock)
+{
+    int pos;
+
+
+    ft_checkaccept(stack_a, stock);
+    pos = stock->tab2[stock->var_tabcheck];
+    if (pos > stock->size_a / 2)
+    {
+        while(pos < stock->size_a)
+        {
+            ft_rra(stack_a);
+            pos++;
+        }
+    }
+    else if (pos < stock->size_a / 2)
+    {
+        while(pos + 1 > 0)
+        {
+            ft_ra(stack_a);
+            pos--;
+        }
+    }
+}
+
+void	ft_checkaccept(t_stack **stack_a, t_stock *stock)
+{
+    int w;
+    int x;
+    int i;
+    int check;
+    int check_pos;
+    t_stack *tmp;
+    
+    w = 20 * stock->checkaccepta;
+    x = 0;
+    i = 0;
+	stock->var_tabcheck = w - 1;
+
+    if (w > stock->size_a)
+        w = stock->size_a;
+    while(w)
+    {
+        check = INT_MAX;
+        tmp = (*stack_a);
+        while (tmp)
+        {
+            if (check > tmp->content && ft_checktab(tmp->content, stock->tab) != 0)
+            {
+                check = tmp->content;
+                check_pos = i;
+            }
+            tmp = tmp->next;
+            i++;
+        }
+        stock->tab[x] = check;
+        stock->tab2[x] = check_pos;
+        x++;
+        w--;
+    }
+    stock->checklast = stock->tab[x];
+    stock->checkaccepta += 1;
+}
+
+void mvlowercoup(int indic, t_stock *stock, t_stack **stack_a)
+{
+    int coups;
+
+    printf("1\n");
+    printf("%d\n", indic);
+    coups = stock->tab2[indic];
+    if(stock->tab3[indic] == 0)
+    {
+        while(coups > 0)
+        {
+            ft_rra(stack_a);
+            coups--;
+        }
+    }
+    else
+    {
+        while(coups > 0)
+        {
+            ft_ra(stack_a);
+            coups--;
+        }
+    }
+}
+
+void ft_crea_tab_pos(t_stack **stack_a, t_stock *stock)
+{
+    int w;
+    int x;
+    int i;
+    int check;
+    int check_pos;
+    t_stack *tmp;
+    
+    w = 20;
+    x = 0;
+    i = 0;
+    stock->done += w;
+    if (stock->done > stock->size_a)
+    {
+        w = w - (stock->done - stock->size_a);
+        stock->done = stock->size_a;
+    }
+    ft_print_lst(*stack_a);
+    while(w)
+    {
+        i = 0;
+        check = INT_MAX;
+        tmp = (*stack_a);
+        while (tmp)
+        {
+            if (check > tmp->content && ft_checktab(tmp->content, stock->tab) != 0)
+            {
+                check = tmp->content;
+                check_pos = i;
+            }
+            tmp = tmp->next;
+            i++;
+        }
+        stock->tab[x] = check;
+        stock->tab2[x] = check_pos;
+        x++;
+        w--;
+    }
+    printf("valeur tab = %d\n", stock->tab[1]);
+    printf("valeur tab2 = %d\n", stock->tab2[1]);
+    stock->checklast = stock->tab[x];
+}
+
+int ft_checktab(int valeur, int *tab)
+{
+    int i;
+
+    i = 0;
+    while (tab[i])
+    {
+        if (tab[i] == valeur)
+            return(0);
+        i++;
+    }
+    return(1);
+}
+
+void nmkcoup(t_stock *stock)
+{
+    int w;
+    int x;
+
+    x = 0;
+    w = 20;
+    if(stock->size_a < 20)
+        w = stock->size_a;
+    while(w)
+    {
+        if(stock->size_a - stock->tab2[x] > stock->tab2[x] - 1) // 1 = ra 
+        {
+            stock->tab2[x] = stock->tab2[x] - 1;
+            stock->tab3[x] = 1;
+        }
+        else // 0 = rra
+        {
+            stock->tab2[x] = (stock->size_a - stock->tab2[x] + 1);
+            stock->tab3[x] = 0;
+        }
+		printf("tab mk = %d\n", stock->tab2[x]);
+		x++;
+        w--;
+    }
+}
+
+int checklowercoup(t_stock *stock)
+{
+    int i;
+    int check;
+    int indic;
+
+    check = INT_MAX;
+    i = 0;
+    while(stock->tab2[i])
+    {
+        if(check > stock->tab2[i])
+        {
+            check = stock->tab2[i];
+            printf("i = %d\n", i);
+            indic = i;
+        }
+        i++;
+    }
+    return(indic);
+}
+
+
+void ft_last(t_stack **stack_b, t_stock *stock)
+{
+	t_stack *tmp;
+
+	tmp = (*stack_b);
+	if (*stack_b == NULL)
+		return ;
+	while(tmp && tmp->next)
+		tmp = tmp->next;
+	stock->lastb = tmp->content;
+}
+
+void ft_pos_accept(t_stack **stack_a, t_stack **stack_b, t_stock *stock)
+{
+	int i;
+	int value;
+	int pos_accept;
+	t_stack *tmp;
+
+	value = (*stack_a)->content;
+	tmp = (*stack_b);
+	i = 1;
+	if (*stack_b == NULL || stock->size_b == 1 
+		|| (value > (*stack_b)->content && value > stock->lastb))
+	{
+        printf("oui\n");
+		ft_pb(stack_a, stack_b);
+		return ;
+	}
+	printf("stack_a = %d\n", (*stack_a)->content);
+	ft_print_lst(*stack_b);
+	if (value < (*stack_b)->content && value < stock->lastb)
+	{
+		ft_pb(stack_a, stack_b);
+		ft_rb(stack_b);
+		return ;
+	}
+	while(tmp)
+	{
+		if(value < tmp->content && value > tmp->next->content)
+			pos_accept = i;
+		tmp = tmp->next;
+		i++;
+	}
+	printf("pos 1\n");
+	if(pos_accept > (stock->size_b / 2))
+	{
+		while (pos_accept < stock->size_b)
+		{
+			pos_accept++;
+			ft_rrb(stack_b);
+		}
+	}
+	else if (pos_accept < (stock->size_b / 2))
+	{
+		while (pos_accept > 0)
+		{
+			pos_accept--;
+			ft_rb(stack_b);
+		}
+	}
+	ft_pb(stack_a, stack_b);
+}
+
+
+
+
+
+
+
+
+
+
+/*
+ft_tri100test()
+{
+    get_size_a();
+    mkchunk();
+    ft_chunk();
+	ft_pos_accept();
+}
+
+
+mkchunk(t_stock *stock)
+{
+    int chunk_size;
+
+    if(stock->size_a > 5 && stock->size_a <= 100)
+    {
+        chunk_size =  20;
+    }
+    else if(size_a > 100)
+    {
+        chunk_size = 25;
+    }
+}
+
+ft_chunk(t_stock *stock, t_stack **stack_a)
+{
+    int i;
+    int check;
+    t_stack *tmp;
+    int verif;
+    int pos;
+
+    verif = 150;    
+    while(stock->chunk_size > i)
+    {
+        i = 0;
+        check = INT_MAX;
+        tmp = (*stack_a);
+        while (tmp)
+        {
+            if (check > tmp->content && checkcp(i, stock) < verif)
+            {
+                check = tmp->content;
+                verif = checkcp(i, stock);
+            }
+            tmp = tmp->next;
+            i++;
+        }
+    }
+    pos = ft_checkpos(check, stack_a);
+    ft_mvfirst(stock, pos, stack_a);
+}
+
+void ft_mvfirst(t_stock *stock, int pos, t_stack **stack_a)
+{
+    if(pos > stock->size_a/2)
+    {
+        while((stock->size_a + 1) - pos)
+        {
+            ft_rra(stack_a);
+            pos++;
+        }
+    }
+    else
+        while(pos)
+        {
+            ft_ra(stack_a);
+            pos--;
+        }
+    }
+}
+
+int ft_checkpos(int check, t_stack **stack_a)
+{
+    int i;
+    t_stack *tmp;
+    int pos;
+
+    tmp = (*stack_a);
+    while (tmp)
+    {
+        if (tmp->content == check)
+            pos = i;
+        i++;
+        tmp = tmp->next;
+    }
+    return(pos);
+}
+int checkcp(int i, t_stock *stock)
+{
+    if(i > stock->size_a /2)
+        i = i - stock->size_a/2;
+    return(i);
+}
+*/
